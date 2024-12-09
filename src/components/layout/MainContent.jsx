0de5d76be30aeb79ca964dc2dbab7f6b280c2518
uttamribadiya16 <uttamribadiya163098@gmail.com>
2024-12-09 22:16:20 +0530

    Updated css for home page

--- src/components/layout/MainContent.jsx
+++ src/components/layout/MainContent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function MainContent({ children }) {
   return (
     <div className="flex-1 theme-bg-primary overflow-auto">
-      <div className="max-w-6xl mx-auto p-6">
+      <div className="max-w-8xl mx-auto p-6">
         {children}
       </div>
     </div>
